901751de78d1fcd165707a7375fb1d6499b93337
Sebastian <sebastian.peralta@unmsm.edu.pe>
2023-05-06 10:04:47 -0500

    Actualizacion
    Se agrego el funcionamiento de guardado de texto en una base de datos

--- usuarios.cpp
+++ usuarios.cpp
@@ -10,8 +10,14 @@ main()
 void NuevoUsuario(){
 	string NewUsu,NewContra;
 	ofstream UsuariosText;
-	UsuariosText.open("Usuarios.csv");	
+	UsuariosText.open("Usuarios.csv",ios::out);//ABRIENDO  BASE DE DATOS;
+	if(UsuariosText.fail()){
+		cout<<"ERROR,el archivo no se pudo crear";
+		exit(1);
+	}	
 	cout<<"\t\t\tREGISTRESE\n";
 	cout<<"Usuario: ";cin>>NewUsu;
 	cout<<"Contraseña: ";cin>>NewContra;
+	UsuariosText<<NewUsu<<";"<<NewContra;
+	UsuariosText.close();
 }
