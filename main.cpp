19e9e242f820a3a07e1e5d3148326f56abfedfe4
Sebastian <sebastian.peralta@unmsm.edu.pe>
2023-06-09 18:23:53 -0500

    FUNCIONA AL 100% AAAA

--- main.cpp
+++ main.cpp
@@ -4,45 +4,11 @@
 #include <ctype.h>
 #include <string.h>
 #include "usuarios.cpp"
-using namespace std;
 
-/*string solve( string s );*/
-int main()
-{
-/////MENU 1
-	char SelecMenu1,la;
-	cout<<"\t\tMENU\n1.- Crear Usuario\n2.- Iniciar sesion\n\n\3.- Salir"<<endl;
-	cin>>SelecMenu1;
-	switch(SelecMenu1)
-	{
-		case '1':
-			//NUEVO USUARIO
-			system("CLS");
-			NuevoUsuario();
-			break;
-		case '2':
-			//USUARIO EXISTENTE
-			system("CLS");
-			LoginUsu();
-			break;
-		default :
-			cout<<"Error,Intente denuevo."<<endl;
-			system("pause");
-			system("CLS");
-	
-	}
-	cout<<SelecMenu1<<endl;
-	cout<<SelecMenu1<<endl;
-//MENU 2
-	
-	system("color 52");
-	system("pause");		
+using namespace std;
+main(){
+	Menu();
 }
-/*string solve( string s ) {
-   for( int i = 0; i < s.length(); i++ ) {
-      if( !isdigit( s[i] )) {
-         return "False";
-      }
-   }
-   return "True";
-}*/
+	
+
+
--- usuarios.cpp
+++ usuarios.cpp
@@ -2,22 +2,67 @@
 #include <sstream>
 #include <fstream>
 #include <string.h>
+
 /*
 #ifndef usuarios_H
 #define usuarios_H
 */
 using namespace std;
+static void CargarEstructuras(void) ;
 static void NuevoUsuario(void);
 static void LoginUsu(void);
-#ifdef InSingleFileTest
-int main(){
-	LoginUsu();
-	return 0;
-};
-#endif
+static void Menu();
+struct Usuarios{
+	string nombre ;
+	string contra ;
+	string tipo ;
+	string moneda;
+}usuario[100];
+int num_usu;
+
+static void CargarEstructuras(int num_usu)
+{
+	num_usu = 0;
+	string linea;
+	char delimit = ';';
+	ifstream UsuariosText;
+	//	ofstream UsuariosText;
+	UsuariosText.open("Usuarios.csv",ios::in);
+	if (!UsuariosText.is_open())
+	{
+	    cout << "Error al abrir ejemplo.dat\n";
+		exit(1);
+	}
+	getline(UsuariosText, linea);
+	while (getline(UsuariosText, linea))
+	{
+		stringstream stream(linea); 
+		string Usuario,Contrasena;
+		getline(stream, Usuario, delimit);
+		getline(stream, Contrasena, delimit);
+		usuario[num_usu].nombre = Usuario;
+		usuario[num_usu].contra = Contrasena;
+		//cout<<usuario[num_usu].nombre<<endl;
+		//cout<<usuario[num_usu].contra<<endl;
+		if(num_usu == 0) {
+			usuario[num_usu].tipo = "admin";
+		}
+		num_usu++;
+	}
+	UsuariosText.close();
+	cout<<num_usu<<endl;
+	//cout<<"Cargando..."<<endl;
+	//
+}
+
+
 static void NuevoUsuario(void)
 {
+	CargarEstructuras(num_usu);
+	//cout<<usuario[0].nombre ;
+	//cout<<"HOLA"<<endl;
 	string NewUsu,NewContra;
+	int RegExist = 0;
 	ofstream UsuariosText;
 	UsuariosText.open("Usuarios.csv",ios::app);//ABRIENDO  BASE DE DATOS// PARA GUARDAR NUEVO USUARIO AL FINAL DE LA LISTA;
 	if(UsuariosText.fail()){
@@ -27,61 +72,103 @@ static void NuevoUsuario(void)
 	cout<<"\t\t\tREGISTRESE\n";
 	cout<<"Usuario: ";cin>>NewUsu;
 	cout<<"Contraseña: ";cin>>NewContra;
-	cout<<"Registrado Correctamente";
-	UsuariosText<<NewUsu<<";"<<NewContra<<endl;
-	UsuariosText.close();
+	for (int i = 0 ; i == num_usu ; i++){
+		if( NewUsu == usuario[i].nombre)
+		{
+			RegExist = 1;
+			break;			
+		}
+	}
+
+	if (RegExist == 0){
+		cout<<"Registrado Correctamente";
+		UsuariosText<<NewUsu<<";"<<NewContra<<endl;
+		UsuariosText.close();
+	}
+	else{
+		cout<<"Usuario Ya existe. Intentelo denuevo"<<endl;
+		UsuariosText.close();
+		system("pause");
+		system("CLS");
+		NuevoUsuario();
+	}
+	
 }
 /////////////////////////////////////////////////////
 static void LoginUsu(void)
 {
-	string usu, contra, linea, lol;
-	int sizetam;
-	char delimit = ';';
-	ifstream UsuariosText;
-	UsuariosText.open("Usuarios.csv",ios::in);//ABRIENDO BASE DE DATOS// PARA LEER USUARIOS
-	
-	if (!UsuariosText.is_open())
-	{
-	    cout << "Error al abrir ejemplo.dat\n";
-		exit(1);
-	}
+	CargarEstructuras(num_usu);
+	//cout<<usuario[1].nombre ;
+	string logusu, logcontra;
+	int LogExist = 0;
 	
 	cout<<"\t\t\tINICIE SESION\n";//Tomando datos para posterior comprobacion de existencia
-	cout<<"Usuario: ";cin>>usu;
-	cout<<"Contraseña: ";cin>>contra;
-	
-	getline(UsuariosText, linea);
-	//sizetam =UsuariosText.size();
-	cout<<sizetam;	
-	// Leemos todas las líneas
-	while (getline(UsuariosText, linea))
+	cout<<"Usuario: ";cin>>logusu;
+	cout<<"Contraseña: ";cin>>logcontra;
+	for (int i = 0 ; i ==num_usu; i++ )
 	{
-		stringstream stream(linea); // Convertir la cadena a un stream
-		string Usuario,Contrasena;
-		// Extraer todos los valores de esa fila
-		sizetam =linea.length();
-		cout<<sizetam<<endl;
-		
-		cout<<linea<<endl;
-		getline(stream, Usuario, delimit);
-		getline(stream, Contrasena, delimit);
-		if(usu == Usuario && contra == Contrasena)
+		if (logusu == usuario[i].nombre && logcontra == usuario[i].contra)
 		{
 			cout<<"Inicio sesion correctamente"<<endl;
-			if (usu == "ADMIN"|| contra == "admin")
+			LogExist = 1;
+			if (usuario[i].tipo == "admin")
 			{
 				//
-				//Mostrar TIENDA DE ADMINISTRADOR
+				//Mostrar TIENDA DE ADMINISTRADOR()
 				//
 				//
 				//
 				//
-			} 
-		}else if(usu == Usuario || contra == Contrasena)
+
+				break;
+			}
+			else
+			{
+				//
+				//
+				//Mostrar TIENDA DE USUARIO()
+				//
+				//
+				//
+				//
+				break;
+			}
+		}
+		else if(logusu == usuario[i].nombre && logcontra != usuario[i].contra)
 		{
-			cout<<"Usuario o contraseña incorrecto"<<endl;
+			cout<<"contraseña incorrecta"<<endl;
+			break;
 		}
 	}
-	UsuariosText.close();	
+	if (LogExist != 1)
+	{
+		cout<<"Usuario no existe"<<endl;
+	}
 }
+void Menu()
+{
+	/////MENU 1
+	char SelecMenu1,la;
+	cout<<"\t\tMENU\n1.- Crear Usuario\n2.- Iniciar sesion\n\n \3.- Salir"<<endl;
+	cin>>SelecMenu1;
+	switch(SelecMenu1)
+	{
+		case '1':
+			//NUEVO USUARIO
+			system("CLS");
+			NuevoUsuario();
+			break;
+		case '2':
+			//USUARIO EXISTENTE
+			system("CLS");
+			LoginUsu();
+			break;
+		default :
+			cout<<"Error,Intente denuevo."<<endl;
+			system("pause");
+			system("CLS");
+	
+	}
 
+	system("pause");		
+}
